b22edc2807f338d0d1a535e5e6988835fd5ed5d5
Winston <yuhui.wang@aquilatech.ai>
2026-02-15 14:52:29 +0000

    v0.2.0

--- skill/src/version.ts
+++ skill/src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated file - do not edit
-export const VERSION = '0.1.0'
-export const GIT_HASH = 'be5b1b3c646c53d5d5d81344024f28d40f599960'
-export const GIT_SHORT = 'be5b1b3'
-export const BUILD_TIME = '2026-02-15T14:42:02.805Z'
+export const VERSION = '0.2.0'
+export const GIT_HASH = 'd7091829b7f6e520f49b7e84863a00b923c954f5'
+export const GIT_SHORT = 'd709182'
+export const BUILD_TIME = '2026-02-15T14:51:16.918Z'
